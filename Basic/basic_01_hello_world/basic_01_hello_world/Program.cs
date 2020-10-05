3b0bba2c11a4f334765afae3daa8a90e6b9d048d
CAZ <asdfg60842@gmail.com>
2020-10-05 17:34:34 +0900

    Basic 1~3 Chapter for C#

---
+++ Basic/basic_01_hello_world/basic_01_hello_world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_01_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We can use a comment with a slash 
+            /*
+              We can use a comment with a slash and *
+            */
+
+            //Displaying "Hello World"
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Basic/basic_02_data_type/basic_02_data_type/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_02_data_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////
+            //Data type//
+            /////////////
+            
+
+            //Bool
+            bool b = true;
+
+            //Numeric
+            short sh = -32768;
+            int i = 2147483647;
+            long l = 1234L;      // L suffix
+            float f = 123.45F;   // F suffix
+            double d1 = 123.45;
+            double d2 = 123.45D; // D suffix
+            decimal d = 123.45M; // M suffix
+
+            // Char/String
+            char c = 'A';
+            string s = "Hello";
+
+            // DateTime  2020-10-4 11:32
+            DateTime dt = new DateTime(2020, 10, 4, 11, 32, 0);
+
+
+            //////////////////
+            //Max, Min Value//
+            //////////////////
+     
+
+            int Max = int.MaxValue;
+            float Min = float.MinValue;
+
+
+            ////////
+            //NULL//
+            ////////
+
+
+            string s_n;
+            s = null;
+
+
+            /////////////////
+            //Nullable type//
+            /////////////////
+
+
+            int? aa = null;
+            aa = 100;
+
+            bool? bb = null;
+
+            Nullable<int> cc = null;
+            cc = 10;
+            int dd = cc.Value;
+        }
+    }
+}
